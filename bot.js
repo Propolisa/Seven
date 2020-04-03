63ee7fc40891cb8e44fb361c0f0891141b3d0133
Chris <null@null.null>
2020-04-03 18:10:18 +0300

    fix

--- bot.js
+++ bot.js
@@ -1,30 +1,20 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
-
-
 client.on('ready', () => {
-
   console.log('I am ready!');
-
 });
 
-
-
 client.on('message', message => {
-
       if (message.content === 'ping') {
         message.reply('pong');
       }
-      if (message.content.toLowerCase().contains("clkbot")) {
+      /*if (message.content.toLowerCase().contains("clkbot")) {
         if (message.content.toLowerCase().contains("who did")) {
           message.reply('@Propolis did it!');
         } else if (message.content.toLowerCase().contains("hi")) {
           message.reply('Hey there! ' + message.author.display_name + ' :)');
-        }
-
+        }*/
       });
 
-
-    client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
