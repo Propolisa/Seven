749c39c84ee2657c3a414cfc1577ef59819cd724
Kris Stanley <31392431+Propolisa@users.noreply.github.com>
2020-04-03 16:56:11 +0300

    Add files via upload

---
+++ clkbot.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
