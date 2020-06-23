9c87f74f98c1b8ccfabff9caed2cd6d32046da19
Chris <null@null.null>
2020-06-23 03:09:38 +0300

    Disable message constructor

--- helpers/discord-message-constructors.js
+++ helpers/discord-message-constructors.js
@@ -1,28 +1,28 @@
-pusherChallengeOwnEvent() = {
-  embed: {
-    color: 3447003,
-    author: {
-      name: "HTB Challenge Own Event",
-      // icon_url: TEAM_STATS.thumb,
-      url: '',
-    },
-    thumbnail: {
-      url: "https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/white/png/24/cogs.png"
-    },
-    description: "**[" + tryDiscordifyUid(unameToUid(username)) + "](http://0)**" + " owned **" + type + "** [" + targetName + "](http://0)" + "!"
-  }
-}
-{
-  embed: {
-    color: 3447003,
-    author: {
-      name: "HTB Challenge Own Event",
-      // icon_url: TEAM_STATS.thumb,
-      url: '',
-    },
-    thumbnail: {
-      url: "https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/white/png/24/cogs.png"
-    },
-    description: "**[" + tryDiscordifyUid(unameToUid(username)) + "](http://0)**" + " owned **" + type + "** [" + targetName + "](http://0)" + "!"
-  }
-}
+// pusherChallengeOwnEvent() = {
+//   embed: {
+//     color: 3447003,
+//     author: {
+//       name: "HTB Challenge Own Event",
+//       // icon_url: TEAM_STATS.thumb,
+//       url: '',
+//     },
+//     thumbnail: {
+//       url: "https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/white/png/24/cogs.png"
+//     },
+//     description: "**[" + tryDiscordifyUid(unameToUid(username)) + "](http://0)**" + " owned **" + type + "** [" + targetName + "](http://0)" + "!"
+//   }
+// }
+// {
+//   embed: {
+//     color: 3447003,
+//     author: {
+//       name: "HTB Challenge Own Event",
+//       // icon_url: TEAM_STATS.thumb,
+//       url: '',
+//     },
+//     thumbnail: {
+//       url: "https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/master/white/png/24/cogs.png"
+//     },
+//     description: "**[" + tryDiscordifyUid(unameToUid(username)) + "](http://0)**" + " owned **" + type + "** [" + targetName + "](http://0)" + "!"
+//   }
+// }
